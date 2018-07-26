203edc0302a5d77408ed9dcb197090c15baba80c
Michał Koperski <37848817+MichalKoperski@users.noreply.github.com>
2018-07-26 20:20:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://safe-river-39436.herokuapp.com/v1/task/';
-  //const trelloApiRoot = 'https://safe-river-39436.herokuapp.com/v1/trello';
+  //const trelloApiRoot = 'https://safe-river-39436.herokuapp.com/v1/task';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
